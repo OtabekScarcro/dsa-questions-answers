7535387cde9fcf59cf764abe7497f54c3181ae6c
Otabek <otaboyev149@gmail.com>
2023-04-09 15:57:06 +0200

    updated-on9thMarch

---
+++ src/TwoSum.java
@@ -0,0 +1,45 @@
+
+
+class pair{
+    long first, second;
+    public pair(long first, long second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int A[] = {1, 2, 4, 5, 7};
+        int B[] = {5, 6, 3, 4, 8};
+        pair[] pairs = allPairs(A, B, A.length, B.length, 9);
+
+    }
+
+    private static pair[] allPairs(int[] A, int[] B, int N, int M, int X) {
+        pair[] pairs = new pair[10];
+        for(int i=0;i<N;i++){
+            for(int )
+        }
+        return pairs;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
